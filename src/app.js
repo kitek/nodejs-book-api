429cc5f2ea7c92c0fa2fdba21682eeb4755ec3f7
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2018-06-18 12:45:55 +0200

    Moved logic to router

--- src/app.js
+++ src/app.js
@@ -1,21 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bookRoutes = require("./bookRoutes");
 const app = express();
-
-const MongoClient = require('mongodb').MongoClient;
-
-// const url = 'mongodb://db:27017/booksapi'; // when using docker-compose for full development
-const url = 'mongodb://localhost:27017/booksapi';
-
-const promise = MongoClient.connect(url);
-const booksPromise = promise.then(function(client) {
-    return client.db().collection("books");
-});
-
-// let books;
-// MongoClient.connect(url, function(err, client) {
-//     books = client.db().collection("books");
-// });
 
 // -- middleware --
 
@@ -33,37 +19,12 @@ app.use(function(req, res, next) {
 
 // -- end points --
 
+app.use("/book", bookRoutes);
+
 app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
-app.post("/book", function(req, res, next) {
-    // destructuring from ES6
-    const {title, authors, isbn, description} = req.body;
-
-    booksPromise.then(function(books) {
-        return books.updateOne(
-            {isbn: isbn},
-            {$set : {title, authors, isbn, description} },
-            {upsert: true}
-        );
-    }).then(function() {
-        res.json({title, authors, isbn, description});    
-    }).catch(next);
-
-});
-
-app.get("/book/:isbn", function (req, res, next) {
-    const isbn = req.params.isbn;
-    booksPromise
-    .then(function(books) {
-        return books.findOne({isbn}, { projection: {_id: 0} });
-    })
-    .then(function(book) {
-        res.json(book);
-    })
-    .catch(next);
-});
 
 // -- error handling --
 
