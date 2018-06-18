4845fcc852211382deef4940ccfdc12d1a3e2db9
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2018-06-18 15:04:35 +0200

    Moved error handling to common wrap functions

--- src/bookController.js
+++ src/bookController.js
@@ -1,23 +1,34 @@
 
 module.exports = function bookControllerFactory({bookService, bookRepository}) {
-    return {
+    return withErrorHandling({
     	async createOrUpdate(req, res, next) {
-    		const book = req.body;
-    	    try {
-                await bookService.createOrUpdate(book);
-                res.redirect("/book/" + book.isbn);
-            } catch (e) {
-                next(e);
-            }
+            const book = req.body;
+            await bookService.createOrUpdate(book);
+            res.redirect("/book/" + book.isbn);
     	},
     	async details(req, res, next) {
-    		try {
-                const isbn = req.params.isbn;
-                const book = await bookRepository.findOne(isbn);
-                res.json(book);
-            } catch(e) {
-                next(e);
-            }
+            const isbn = req.params.isbn;
+            const book = await bookRepository.findOne(isbn);
+            res.json(book);
     	}
-    };
+    });
 };
+
+function withErrorHandling(api) {
+    const apiWithErrorHandling = {};
+    Object.keys(api).forEach(function(key) {
+        const originalFn = api[key];
+        apiWithErrorHandling[key] = wrapWithTryCatch(originalFn);
+    });
+    return apiWithErrorHandling;
+}
+
+function wrapWithTryCatch(fn) {
+    return async function(req, res, next) {
+        try {
+            return await fn(req, res);
+        } catch(e) {
+            next(e);
+        }
+    }
+}
